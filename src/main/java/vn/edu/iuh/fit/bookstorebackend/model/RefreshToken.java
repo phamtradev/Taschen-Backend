74d736f447fd859edd761bd2f2bb6a2c12a54344
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-09 21:59:10 +0700

    feat(auth): add refresh token entity

---
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/model/RefreshToken.java
@@ -0,0 +1,33 @@
+package vn.edu.iuh.fit.bookstorebackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token", nullable = false, length = 500)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "expires_at", nullable = false)
+    private Instant expiresAt;
+
+    // logout -> revoked = true
+    @Column(name = "revoked", nullable = false)
+    private boolean revoked = false;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+}
