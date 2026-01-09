f384fd8fe9b2542831373e45d4ceb24c2f7f6a00
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-09 22:01:21 +0700

    feat(auth): implement auth endpoints and service

---
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/controller/AuthController.java
@@ -0,0 +1,9 @@
+package vn.edu.iuh.fit.bookstorebackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+}
