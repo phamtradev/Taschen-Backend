98fff67917d42e3320b18be27147f25bfb768014
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-09 19:45:45 +0700

    initial commit

---
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/BookstorebackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.bookstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/bookstorebackend/BookstorebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.bookstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
