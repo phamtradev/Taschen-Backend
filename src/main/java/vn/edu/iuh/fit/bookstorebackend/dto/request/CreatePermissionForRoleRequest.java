cac5d79a8bc6eca7fb88a95972e58f048c72fe23
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-10 14:58:52 +0700

    config: permission request

---
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/dto/request/CreatePermissionForRoleRequest.java
@@ -0,0 +1,17 @@
+package vn.edu.iuh.fit.bookstorebackend.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import vn.edu.iuh.fit.bookstorebackend.common.HttpMethod;
+
+@Getter
+@Setter
+public class CreatePermissionForRoleRequest {
+    private String roleCode;
+    private HttpMethod httpMethod;
+    private String pathPattern;
+    private Boolean active;
+}
+
+
+
