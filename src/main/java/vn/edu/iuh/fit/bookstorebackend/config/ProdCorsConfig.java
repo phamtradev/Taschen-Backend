957366205bb6e3b7a26b1c3605d3f3f69960df3d
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-13 19:36:02 +0700

    update: config cors error

---
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/config/ProdCorsConfig.java
@@ -0,0 +1,42 @@
+package vn.edu.iuh.fit.bookstorebackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@Profile("prod")
+public class ProdCorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        // frontend deploy trên Vercel
+        config.setAllowedOriginPatterns(List.of(
+//                "https://*.vercel.app"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of(
+                "Authorization",
+                "Content-Type"
+        ));
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
