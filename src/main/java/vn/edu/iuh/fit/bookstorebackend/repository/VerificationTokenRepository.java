488d6d1b26f437d6d4d8251a8f36aef498a20bad
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-16 22:18:34 +0700

    update: logic verifytoken if user not verify then delete the current register

--- src/main/java/vn/edu/iuh/fit/bookstorebackend/repository/VerificationTokenRepository.java
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/repository/VerificationTokenRepository.java
@@ -2,14 +2,18 @@ package vn.edu.iuh.fit.bookstorebackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import vn.edu.iuh.fit.bookstorebackend.model.User;
 import vn.edu.iuh.fit.bookstorebackend.model.VerificationToken;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
     Optional<VerificationToken> findByToken(String token);
     void deleteByToken(String token);
+    List<VerificationToken> findByUser(User user);
+    void deleteByUser(User user);
 }
 
 
