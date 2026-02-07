babbdbbf24dd1f4f8c9102f74bfe75b0d76f9453
Phạm Trà <phamvantra.dev@gmail.com>
2026-02-08 01:26:48 +0700

    feat: add StockRequest request DTOs
    - CreateStockRequestRequest
    - ApproveStockRequestRequest
    - RejectStockRequestRequest

---
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/dto/request/RejectStockRequestRequest.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.bookstorebackend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RejectStockRequestRequest {
+    private Long processedById;
+    private String responseMessage;
+}
