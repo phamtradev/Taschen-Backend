d2afef5abbce6e2ea341a06bfa218ef0c88e8d25
Phạm Trà <phamvantra.dev@gmail.com>
2026-03-03 01:36:43 +0700

    feat: add functionality to create purchase orders from approved stock requests, including validation and service implementation

--- src/main/java/vn/edu/iuh/fit/bookstorebackend/dto/request/CreatePurchaseOrderFromStockRequestRequest.java
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/dto/request/CreatePurchaseOrderFromStockRequestRequest.java
@@ -1,16 +1,18 @@
 package vn.edu.iuh.fit.bookstorebackend.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateStockRequestRequest {
-    private Long bookId;
-    private Long variantId;
-    private int quantity;
-    private String reason;
+public class CreatePurchaseOrderFromStockRequestRequest {
+    private Long stockRequestId;
+    private Long supplierId;
     private Long createdById;
+    private double importPrice;
+    private String note;
 }
