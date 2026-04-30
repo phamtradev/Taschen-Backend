ed0c2ddfedb2d1f7bb81e01eca22625c145505e0
Phạm Trà <phamvantra.dev@gmail.com>
2026-05-01 01:23:49 +0700

    feat(user): add @EntityGraph import to UserRepository for optimized role fetching

--- src/main/java/vn/edu/iuh/fit/bookstorebackend/user/repository/UserRepository.java
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package vn.edu.iuh.fit.bookstorebackend.user.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
