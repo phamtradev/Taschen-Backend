e98d071b1fe4d0ccbafb5e42db2a8dfcad183cdc
Phạm Trà <phamvantra.dev@gmail.com>
2026-05-01 00:47:27 +0700

    feat(user): enhance Role and User repositories with permissions fetching
    - Updated RoleRepository to include a query for fetching roles with permissions by ID.
    - Modified UserRepository to fetch users with their roles and associated permissions in queries.

--- src/main/java/vn/edu/iuh/fit/bookstorebackend/user/repository/RoleRepository.java
+++ src/main/java/vn/edu/iuh/fit/bookstorebackend/user/repository/RoleRepository.java
@@ -1,6 +1,8 @@
 package vn.edu.iuh.fit.bookstorebackend.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.edu.iuh.fit.bookstorebackend.user.model.Role;
 
@@ -12,8 +14,12 @@ import java.util.Set;
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByCode(String code);
 
-    Set<Role> findByIdIn(Set<Long> ids);
+    @Query("SELECT DISTINCT r FROM Role r LEFT JOIN FETCH r.permissions WHERE r.id IN :ids")
+    Set<Role> findByIdIn(@Param("ids") Set<Long> ids);
     
     List<Role> findByPermissionsId(Long permissionId);
+    
+    @Query("SELECT DISTINCT r FROM Role r LEFT JOIN FETCH r.permissions WHERE r.id = :id")
+    Optional<Role> findByIdWithPermissions(@Param("id") Long id);
 }
 
